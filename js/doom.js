a9c6f4c1022b566b15f5e6021b35d1f86c166eb9
Ricardo  Torres <torresliendo678@gmail.com>
2022-08-11 16:52:09 -0400

    Se agrega el proyecto

---
+++ js/doom.js
@@ -0,0 +1,5 @@
+let elemento;
+elemento = document.head;
+
+
+console.log(elemento);
---
+++ js/main.js
@@ -0,0 +1,4 @@
+let boton=document.querySelector("#theme");
+boton.addEventListener('click',()=>{
+    document.body.classList.toggle('dark');
+});
